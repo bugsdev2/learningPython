43748019d626c905ed7c7a05c9d1845af23fd056
Aadi <adithyamanikumar@proton.me>
2024-07-17 23:46:43 +0530

    Random changes

--- Day2_tip_calculator/tip_calculator.py
+++ Day2_tip_calculator/tip_calculator.py
@@ -1,9 +1,10 @@
 def calc_tip_per_person(total, tip, persons):
-	# ~ basically instead of doing bill amount + (bill amount * tip percent (12/100 = .12)), 
-	# ~ we do bill amount * 1 + 0.12 => 1.12 to get total.
-	tip_ease = "1." + tip 
-	# ~ "{:.2f}".format(***) is used to round to two decimal places proper
-	return "{:.2f}".format((float(total) * float(tip_ease) ) / int(persons), 2)
+    # ~ basically instead of doing bill amount + (bill amount * tip percent (12/100 = .12)),
+    # ~ we do bill amount * 1 + 0.12 => 1.12 to get total.
+    tip_ease = "1." + tip
+    # ~ "{:.2f}".format(***) is used to round to two decimal places proper
+    return "{:.2f}".format((float(total) * float(tip_ease)) / int(persons), 2)
+
 
 print("Tip Calculator")
 bill_amount = input("Enter the total bill: Rs.")
@@ -13,4 +14,3 @@ person_count = input("How many people are splitting the bill? ")
 each_person_amount = calc_tip_per_person(bill_amount, tip_amount, person_count)
 
 print(f"Each person has to pay Rs.{each_person_amount}/-")
-
